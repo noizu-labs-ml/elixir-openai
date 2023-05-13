77fd0346de68590947b45867d2c178333ff0cf78
Keith Brings <keith.brings@noizu.com>
2023-05-14 02:44:29 +0700

    tweaks for hex

--- mix.exs
+++ mix.exs
@@ -5,11 +5,31 @@ defmodule Noizu.OpenAI.MixProject do
     [
       app: :noizu_labs_open_ai,
       name: "Noizu Labs: OpenAI",
+      description: description(),
+      package: package(),
       version: "0.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+    ]
+  end
+
+
+  defp description() do
+    "OpenAI Wrapper"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        project: "https://github.com/noizu-labs/elixir-openai",
+        noizu_labs: "https://github.com/noizu-labs",
+        noizu_labs_machine_learning: "https://github.com/noizu-labs-ml",
+        noizu_labs_scaffolding: "https://github.com/noizu-labs-scaffolding",
+        developer_github: "https://github.com/noizu"
+      }
     ]
   end
 
@@ -34,8 +54,8 @@ defmodule Noizu.OpenAI.MixProject do
     [
       {:finch, "~> 0.15"},
       {:jason, "~> 1.2"},
-      {:mimic, "~> 1.0.0", only: :test}
-
+      {:mimic, "~> 1.0.0", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
